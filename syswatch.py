1b21c2d328dd2f67a3cd5b0e244d184b8c35e8a9
Ripple <zeeurgenz@gmail.com>
2026-06-03 17:49:00 -0500

    syswatch: clean Ctrl+C exit

--- syswatch.py
+++ syswatch.py
@@ -13,19 +13,18 @@ def get_operator():
 operator = get_operator()
 subprocess.run('figlet SYSWATCH | lolcat', shell=True)
 while True:
-    os.system('clear')
-    print('===SYSWATCH - Operator: ' + operator + ' ===')
-    
-    cpu = psutil.cpu_percent(interval=2)
-    print("CPU Usage: " + str(cpu) +'%')
-    
-    ram = psutil.virtual_memory()
-    ram_used = round(ram.used / (1024**3), 1)
-    ram_total = round(ram.total / (1024**3), 1)
-    ram_percent = ram.percent
-    print("RAM: " + str(ram_used) + "GB / " + str(ram_total) + "GB (" +str(ram_percent) + "%)")
-    
-    now = datetime.now().strftime("%H:%M:%S")
-    print("Last Update: " + now)
-    
-    time.sleep(1)
+    try:
+        os.system('clear')
+        print('===SYSWATCH - Operator: ' + operator + ' ===')
+        cpu = psutil.cpu_percent(interval=2)
+        print("CPU Usage: " + str(cpu) +'%')
+        ram = psutil.virtual_memory()
+        ram_used = round(ram.used / (1024**3), 1)
+        ram_total = round(ram.total / (1024**3), 1)
+        ram_percent = ram.percent
+        print("RAM: " + str(ram_used) + "GB / " + str(ram_total) + "GB (" +str(ram_percent) + "%)")
+        now = datetime.now().strftime("%H:%M:%S")
+        print("Last Update: " + now)
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
