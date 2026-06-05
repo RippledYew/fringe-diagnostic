98e1928a98d050be9267b459ab8139cb98be1b7a
Ripple <zeeurgenz@gmail.com>
2026-06-05 10:50:29 -0500

    refactor fringe_nodes to use FringeNode class

--- fringe_node.py
+++ fringe_node.py
@@ -1,16 +1,21 @@
 class FringeNode:
-    def __init__(self, name, status, ip, role):
+    def __init__(self, name, status, ip, role, os, cpu, cores, ram_gb):
         self.name = name
         self.status = status
         self.ip = ip
         self.role = role
+        self.os = os
+        self.cpu = cpu
+        self.cores = cores
+        self.ram_gb = ram_gb
     def report(self):
-        print(f"Node: {self.name} | Status: {self.status} | IP: {self.ip} | Role: {self.role}")
+        print(f"Node: {self.name} | Status: {self.status} | IP: {self.ip}")
+        print(f"Role: {self.role} | OS: {self.os} | Cores: {self.cores} | RAM: {self.ram_gb}GB")
         
 if __name__ == "__main__":
-    acer = FringeNode("Acer", "online", "192.168.1.50", "Learn-to-code node")
-    frank = FringeNode("Frank", "pending", "TBD", "Primary ECFM compute node")
-    egor = FringeNode("Egor", "pending", "TBD", "Watchdog")
+    acer = FringeNode("Acer", "online", "192.168.1.50", "learn", "ubuntu 22.04", "intel if-7200U", 4, 16)
+    frank = FringeNode("Frank", "pending", "TBD", "compute", "ubuntu 24.04", "Dual Xeon e5-2699 v4", 44, 256)
+    egor = FringeNode("Egor", "pending", "TBD", "watchdog", "armbian", "RK3588S", 8, 32)
     print(acer.name)
     print(acer.status)
     acer.report()
--- fringe_nodes.py
+++ fringe_nodes.py
@@ -1,36 +1,14 @@
 #1/usr/bin/env python3
 
+from fringe_node import FringeNode
+
 cluster = {
-    "Acer": {
-        "ip": "10.58.163.167",
-        "role": "learn",
-        "os": "ubuntu 22.04",
-        "cpu": "intel i5-7200U",
-        "cores": 4,
-        "ram_gb": 16,
-        "status": "online"
-    },
-    "Frank": {
-        "ip": "TBD",
-        "role": "compute",
-        "os": "Ubuntu 24.04",
-        "cpu": "Dual Xeon e5-2699v4",
-        "cores": 88,
-        "ram_gb": 256,
-        "status": "pending"
-    },
-    "Egor": {
-        "ip": "TBD",
-        "role": "watchdog",
-        "os": "Armbian",
-        "cpu": "RK3588S",
-        "cores":8,
-        "ram_gb": 16,
-        "status": "pending"
-    }
+    "Acer": FringeNode("Acer", "online", "10.58.163.167", "learn", "ubuntu 22.04", "intel i5-7200U", 4, 16),
+    "Frank": FringeNode("Frank", "pending", "TBD", "compute", "ubuntu 22.04", "dual xeon e5 2699 v4", 44, 256),
+    "Egor": FringeNode("Egor", "pending", "TBD", "watchdog", "armbian", "RK3588S", 8, 16),
 }
 
 if __name__ == "__main__":
     for name, info in cluster.items():
-        print(f"{name} | {info['role']} | {info['ip']} | {info['status']}")
+        print(f"{name} | {info.role} | {info.ip} | {info.status}")
         
